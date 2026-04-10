f3cca945e2744c497f7896949a42dfded6d51f83
Javier Casa <mosanbike1256@gmail.com>
2026-04-09 23:14:39 -0400

    agregue a la pagina practica.js los ejercicios del libro de javascript

--- 02-operadores/04-logicos.js
+++ 02-operadores/04-logicos.js
@@ -2,18 +2,26 @@
 
 //and &&
 
-let mayor = false;
-let suscrito =true;
-// console.log(true && true);
-// console.log(false && true);
-console.log('operador and',mayor && suscrito);
+// let mayor = false;
+// let suscrito =true;
+// // console.log(true && true);
+// // console.log(false && true);
+// console.log('operador and',mayor && suscrito);
 
-//or ||
-console.log('operador or',mayor || suscrito);
+// //or ||
+// console.log('operador or',mayor || suscrito);
 
-// NOT !
+// // NOT !
 
-console.log('operdor not', !mayor);
+// console.log('operdor not', !mayor);
 
-let SoloCatalogoInfantil = !mayor;
+// let SoloCatalogoInfantil = !mayor;
  
+// true && false   // false (AND - ambos deben ser true)
+// true && true    // true
+
+// false || true   // true (OR - al menos uno true)
+// false || false  // false
+
+// !true           // false (NOT - invierte el valor)
+// !false          // true
